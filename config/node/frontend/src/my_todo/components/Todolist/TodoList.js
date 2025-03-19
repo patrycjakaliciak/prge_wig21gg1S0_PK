512c2aa1771d0dd790077b3f512cd7902e788640
patrycjakaliciak <patrycja.kaliciak@student.wat.edu.pl>
2025-03-19 11:24:56 +0100

    costam

---
+++ config/node/frontend/src/my_todo/components/Todolist/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TodoNewTodo({ listHandler, itemHandler }) {
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          itemHandler(e);
+        }}
+      ></input>
+      <button onClick={listHandler}>Dodaj</button>
+    </div>
+  );
+}
